44a2d9896d41c74f3adbbcf23056c87abff7d4ed
20181290 HanChanHui <78011635+hanchanhui@users.noreply.github.com>
2021-09-17 04:43:56 +0000

    SDL-image(3주차)

---
+++ Game.h
@@ -0,0 +1,29 @@
+#include "SDL.h"
+
+class Game{
+  public:
+    Game() {}
+    ~Game() {}
+
+    bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+    void render();
+    void update();
+    bool running();
+    void handleEvent();
+    void clean();
+
+  private:
+    SDL_Window* m_pWindow;
+    SDL_Renderer* m_pRenderer;
+    bool m_bRunning;
+
+  SDL_Texture* m_pTexture;
+
+  //원본 사각형
+  SDL_Rect m_sourceRectangle;
+
+  //대상 사각형
+  SDL_Rect m_destinationRectangle;
+
+};
+
--- main.cpp
+++ main.cpp
@@ -1,66 +1,19 @@
-#include <SDL.h>
-#include <iostream>
-#include <ctime>
-
-/*변경사항
-1초마다 랜덤으로 색깔이 바뀌게 만들었습니다.
-카운터를 만들어 주어 색깔이 5번 바뀐 후 크기가 바뀌게 만들었습니다
-*/
-
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-bool g_bRunning = false;
-
-bool init(const char* title, int xpos, int ypos, int height, int width, int flags){
-  if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-  {
-    g_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
-
-    if(g_pWindow != 0){
-      g_pRenderer = SDL_CreateRenderer(g_pWindow, -1,0);
-    }
-  }
-  else{
-    return false;
-  }
-  
-  return true;
-}
-
-void ColorChange(){// 색깔을 랜덤으로 바꾸었습니다
-  SDL_SetRenderDrawColor(g_pRenderer, rand() % 255, rand() % 255, rand() % 255, 255);
-  SDL_Delay(1000);
-}
-
-void render(){
-  SDL_RenderClear(g_pRenderer); 
-  SDL_RenderPresent(g_pRenderer);
-}
-
+#include "Game.h"
+Game* g_game = 0;
 
 
 int main(int argc, char* args[])
 {
-  int count = 0;
-
-  if(init("HelloSDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 100, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE)){
-    g_bRunning = true;
-  }
-  else{
-    return 1;
-  }
+  g_game = new Game();
   
-
-  while(g_bRunning){
-    if(count == 5){ // 카운터가 5가 넘었을때 크기가 바뀌게끔 만들었습니다.
-      SDL_SetWindowSize(g_pWindow, 640, 480);
-    }
+  g_game->init("Game Class", 100, 100, 110, 85, 0);
   
-    ColorChange();// 색깔 함수를 호출 합니다
-    render();
-    count++;
+  while(g_game->running())
+  {
+    g_game->handleEvent();
+    g_game->update();
+    g_game->render();
   }
-
-  SDL_Quit();
+  g_game->clean();
   return 0;
 }
